83bc891b2c7e3111a7e2a00e0356967a273b029c
Moshe-Morad <41563997+Moshe-Morad@users.noreply.github.com>
2024-08-03 12:00:54 +0300

    Add files via upload

---
+++ Day 1/main-2.py
@@ -0,0 +1,13 @@
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# name = input("What is your name?")
+# print(name)
+
+# print( len( input("What is your name? ")))
+user_name = input("What is your name? ")
+length = len(user_name)
+print(length)
