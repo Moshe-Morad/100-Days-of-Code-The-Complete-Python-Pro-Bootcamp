74fd8bb075e41d3e0fa9cb1824cdcec15d1efba9
Moshe-Morad <41563997+Moshe-Morad@users.noreply.github.com>
2024-08-30 00:49:01 +0300

    Add files via upload

---
+++ Day 19/Listeners/main.py
@@ -0,0 +1,38 @@
+import turtle
+
+tim = turtle.Turtle()
+screen = turtle.Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def move_right():
+    tim.right(10)
+
+
+def move_left():
+    tim.left(10)
+
+
+def clear():
+    # one way to make it happened
+    # tim.home()
+    # tim.clear()
+
+    # second way to make it happened
+    tim.reset()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
